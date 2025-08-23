54b7ae84fe6b8c48821e1a16744956358d497283
yisakm9 <192582247+yisakm9@users.noreply.github.com>
2025-08-23 16:39:05 +0300

    Update outputs.tf to use awscc resources for bot and bot alias IDs

--- modules/lex_bot/outputs.tf
+++ modules/lex_bot/outputs.tf
@@ -1,10 +1,11 @@
+# modules/lex_bot/outputs.tf
 
 output "bot_id" {
   description = "The unique ID of the Lex bot."
-  value       = aws_lexv2models_bot.translation_bot.id
+  value       = awscc_lex_bot.translation_bot.id
 }
 
 output "bot_alias_id" {
   description = "The unique ID of the bot alias."
-  value       = aws_lexv2models_bot_alias.live.bot_alias_id
+  value       = awscc_lex_bot_alias.live.id
 }
